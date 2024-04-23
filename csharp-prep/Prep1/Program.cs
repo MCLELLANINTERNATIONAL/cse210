c7326dbb3662f17bc5ebf275fdd075ee13e89bb6
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2024-04-23 12:09:26 +0100

    added exclamation points and additional sentence

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hello World!!! "); //Added exclamation points"
+        Console.WriteLine("We are pleased to meet today."); //Added sentence and comment  
     }
 }
