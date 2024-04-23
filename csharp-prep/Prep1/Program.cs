e3cfe15dc7cca95a5f3e825bed887f145fa5e6af
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2024-04-23 12:23:20 +0100

    Added new line

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,5 +6,6 @@ class Program
     {
         Console.WriteLine("Hello World!!! "); //Added exclamation points"
         Console.WriteLine("We are pleased to meet today."); //Added sentence and comment  
+        Console.WriteLine("We have several new products to introduce today!"); //Added new line
     }
 }
