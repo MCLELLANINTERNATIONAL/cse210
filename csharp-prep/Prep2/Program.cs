90262a02a82422c78d8f99504a9c5c45dad8c8cf
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2024-04-24 22:15:20 +0100

    Added conditional statements if else if else

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? "); // PUT "Enter the grade"
+        string answer = Console.ReadLine();
+        int percent = int.Parse(Console.ReadLine());
+        
+        string grade = "";
+
+        if (percent >= 90)
+        {
+            grade = "A";
+        }
+        else if (percent >=80 && percent < 90)
+        {
+           grade = "B"; 
+        }
+        else if (percent >=70 && percent < 80)
+        {
+           grade = "C";
+        }
+        else if (percent >=60 && percent < 70)
+        {
+           grade = "D"; 
+        }
+        else
+        {
+           grade = "F";
+        }
+
+            Console.WriteLine($"Your grade is: {grade}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }     
     }
 }
