1e74fe652a0360b768247bc24cbbfe0c72610a17
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2024-04-24 21:21:57 +0100

    Added new lines

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,8 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!!! "); //Added exclamation points"
-        Console.WriteLine("We are pleased to meet today."); //Added sentence and comment  
-        Console.WriteLine("We have several new products to introduce today!"); //Added new line
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();  
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}. "); 
     }
 }
