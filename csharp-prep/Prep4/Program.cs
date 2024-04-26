42b28209c72c0d0fbaeff30bbf51722cbf78a22a
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2024-04-26 21:25:46 +0100

    Completed Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -7,18 +7,18 @@ class Program
     {
         List<int> numbers = new List<int>();
 
-        int userName = -1;
-        while (userName != 0)
+        int userNumber = -1;
+        while (userNumber != 0)
         {
             Console.Write("Enter a number (0 to quit): ");
             string userResponse = Console.ReadLine();
-            int userNumber = int.Parse(userResponse);
+            userNumber = int.Parse(userResponse);
 
-            if (userNumber !=0)
+            if (userNumber != 0)
             {
                 numbers.Add(userNumber);
             }
-        
+
             else if (numbers.Count > 0)
             //1: Compute the sum total of the list 
             {
@@ -28,23 +28,41 @@ class Program
                     sum += number;
                 }
 
-            Console.WriteLine($"The sum is {sum} ");
+                Console.WriteLine($"The sum is {sum} ");
 
-            //2.Compute the average number of the list
-            float average = ((float)sum) / numbers.Count; 
-            Console.WriteLine($"The average is: {average} ");
+                //2.Compute the average number of the list
+                float average = ((float)sum) / numbers.Count;
+                Console.WriteLine($"The average is: {average} ");
 
-            //3. Comopute max number
-            int max = numbers[0];
-            foreach (int number in numbers)
-            {
-            if (number > max)
+                //3. Comopute max number
+                int max = numbers[0];
+                foreach (int number in numbers)
                 {
-                    max = number;
+                    if (number > max)
+                    {
+                        max = number;
+                    }
                 }
+                Console.WriteLine($"The max is: {max}");
+
+                int min = int.MaxValue;
+                foreach (int number in numbers)
+                {
+                    if (number <  min && number > 0)                    
+                    {     
+                        min = number;
+                    }
+                }
+                Console.WriteLine($"The smallest positive number is: {min}");
+
+                numbers.Sort();
+                foreach  (int number in numbers)
+                {
+                    Console.WriteLine(number);
+                }                            
             }
-            Console.WriteLine($"The max is: {max}");
-            }
+
+
             else
             {
                 Console.WriteLine("No numbers were entered.");
