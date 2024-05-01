3fb3b1b487a017fa40bc7e91e3e9b93a3a50caa2
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2024-05-01 15:17:50 +0100

    Developer2 framework completed

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Entry
+{
+    public string _date;
+    public string _ptomptText;
+    public string _entryText;
+
+    public void Display()
+    {
+        
+    }  
+}
