eb10c252e628360672d6e3cbae0a55efd296485c
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2024-05-04 15:54:21 +0100

    update

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Entry
+{
+    public string _date;
+    public string _promptText;
+    public string _entryText;
+    public string _dailyScripture;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_date} : {_promptText}");
+        Console.WriteLine($"Scripture of the Day: {_dailyScripture}");
+        Console.WriteLine(_entryText);
+    }
+}
