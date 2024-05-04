4ca678e54d75a2bac8702a5b393a0d29b4064e67
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2024-05-04 16:59:17 +0100

    added line below comment lines

--- prove/Develop02/ScriptureGenerator.cs
+++ prove/Develop02/ScriptureGenerator.cs
@@ -3,6 +3,7 @@ using System. Collections.Generic;
 using System.IO;
 
 //DailyScripture class provides functionality to generate a random scripture for the day
+
 public class DailyScripture
 {
     private List<string> _scriptures = new List<string>
@@ -24,6 +25,7 @@ public class DailyScripture
     public Random _random = new Random();
 
     // Method to get a random scripture from the list
+    
     public string GetRandomScripture()
     {
         int index = _random.Next(_scriptures.Count);
