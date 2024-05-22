1b5b69ba6cf5431ca9a975b7269ea14d9e5c4fa4
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2024-05-22 13:58:45 +0100

    Completed L5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 3),
+            new Rectangle("Blue", 4, 5),
+            new Circle("Green", 2)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape: {shape.GetType().Name}, Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+        }
     }
 }
