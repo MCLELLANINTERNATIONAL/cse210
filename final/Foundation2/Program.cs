cc4168734f3ee3be983bed8752bd56b1f31925a0
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2024-05-29 00:10:12 +0100

    Final Update F2

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -18,38 +18,38 @@ class Program
         order1.AddProduct(new Product("G456", "Table", 5.00m, 5));
 
         var order2 = new Order(customer2);
-        order2.AddProduct(new Product("W123", "Chair", 3.50m, 3));
         order2.AddProduct(new Product("G456", "Table", 5.00m, 2));
+        order2.AddProduct(new Product("W123", "Chair", 3.50m, 3));
 
         var order3 = new Order(customer3);
-        order3.AddProduct(new Product("Chair", "W123", 3.50m, 3));
-        order3.AddProduct(new Product("G456", "Table", 5.00m, 2));
+        order3.AddProduct(new Product("W123","Chair",  3.50m, 3));
         order3.AddProduct(new Product("S234", "Cover", 1.50m, 3));
+        order3.AddProduct(new Product("G456", "Table", 5.00m, 2));
 
-        Console.WriteLine("Order 1:");
+        Console.WriteLine("\nOrder 1:");
         Console.WriteLine("Total Cost: $" + order1.CalculateTotalCost());
-        Console.WriteLine("Packing Label:");
+        Console.WriteLine("\nPacking Label:");
         Console.WriteLine("---------------------------");
         Console.WriteLine(order1.GetPackingLabel());
-        Console.WriteLine("Shipping Label:");
+        Console.WriteLine("\nShipping Label:");
         Console.WriteLine("---------------------------");
         Console.WriteLine(order1.GetShippingLabel());
         
-        Console.WriteLine("\nOrder 2:");
+        Console.WriteLine("\n\nOrder 2:");
         Console.WriteLine("Total Cost: $" + order2.CalculateTotalCost());
-        Console.WriteLine("Packing Label:");
+        Console.WriteLine("\nPacking Label:");
         Console.WriteLine("---------------------------");
         Console.WriteLine(order2.GetPackingLabel());
-        Console.WriteLine("Shipping Label:");
+        Console.WriteLine("\nShipping Label:");
         Console.WriteLine("---------------------------");
         Console.WriteLine(order2.GetShippingLabel());
 
-        Console.WriteLine("\nOrder 3:");
+        Console.WriteLine("\n\nOrder 3:");
         Console.WriteLine("Total Cost: $" + order3.CalculateTotalCost());
-        Console.WriteLine("Packing Label:");
+        Console.WriteLine("\nPacking Label:");
         Console.WriteLine("---------------------------");
         Console.WriteLine(order3.GetPackingLabel());
-        Console.WriteLine("Shipping Label:");
+        Console.WriteLine("\nShipping Label:");
         Console.WriteLine("---------------------------");
         Console.WriteLine(order3.GetShippingLabel());
     }
