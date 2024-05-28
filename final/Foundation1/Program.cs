09b9322be9954cf41f87e843b824fdd0e36d3351
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2024-05-28 22:52:12 +0100

    Update F1

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -13,14 +13,18 @@ class Program
 
         videos[0].AddComment(new Comment("Kate:", "Great introduction!"));
         videos[0].AddComment(new Comment("Henry:", "Very helpful, thanks!"));
+        videos[0].AddComment(new Comment("Bill:", "Very informative!"));
 
         videos[1].AddComment(new Comment("Alice:", "So many good tips!"));
         videos[1].AddComment(new Comment("Bob:", "Clear and concise."));
+        videos[1].AddComment(new Comment("Cindy:", "Need to watch it again, as there are so many tips!"));
 
         videos[2].AddComment(new Comment("Charlie:", "Now I get interfaces!"));
         videos[2].AddComment(new Comment("Dana:", "Perfect explanation."));
+        videos[2].AddComment(new Comment("Tracey:", "Now I understand abstraction!"));
 
-        foreach (var video in videos) {
+        foreach (var video in videos) 
+        {
             video.DisplayVideoInfo();
             Console.WriteLine();
         }
