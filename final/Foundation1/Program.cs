fb6c28163a95b31f63aed0b01833ef8a7b2befe5
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2024-05-28 22:53:57 +0100

    update 2 F1

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -11,17 +11,17 @@ class Program
             new Video("C# Interfaces Explained", "Tech Educator", 480)
         };
 
-        videos[0].AddComment(new Comment("Kate:", "Great introduction!"));
-        videos[0].AddComment(new Comment("Henry:", "Very helpful, thanks!"));
-        videos[0].AddComment(new Comment("Bill:", "Very informative!"));
+        videos[0].AddComment(new Comment("Kate", "Great introduction!"));
+        videos[0].AddComment(new Comment("Henry", "Very helpful, thanks!"));
+        videos[0].AddComment(new Comment("Bill", "Very informative!"));
 
-        videos[1].AddComment(new Comment("Alice:", "So many good tips!"));
-        videos[1].AddComment(new Comment("Bob:", "Clear and concise."));
-        videos[1].AddComment(new Comment("Cindy:", "Need to watch it again, as there are so many tips!"));
+        videos[1].AddComment(new Comment("Alice", "So many good tips!"));
+        videos[1].AddComment(new Comment("Bob", "Clear and concise."));
+        videos[1].AddComment(new Comment("Cindy", "Need to watch it again, as there are so many tips!"));
 
-        videos[2].AddComment(new Comment("Charlie:", "Now I get interfaces!"));
-        videos[2].AddComment(new Comment("Dana:", "Perfect explanation."));
-        videos[2].AddComment(new Comment("Tracey:", "Now I understand abstraction!"));
+        videos[2].AddComment(new Comment("Charlie", "Now I get interfaces!"));
+        videos[2].AddComment(new Comment("Dana", "Perfect explanation."));
+        videos[2].AddComment(new Comment("Tracey", "Now I understand abstraction!"));
 
         foreach (var video in videos) 
         {
