84bd8b760772a2851bb9ac720bd856bf867cca31
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2024-05-28 16:49:01 +0100

    draft F1

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        List<Video> videos = new List<Video> {
+            new Video("Introduction to C#", "Programming Academy", 300),
+            new Video("Advanced C# Techniques", "Code Guru", 540),
+            new Video("C# Interfaces Explained", "Tech Educator", 480)
+        };
+
+        videos[0].AddComment(new Comment("John Doe", "Great introduction!"));
+        videos[0].AddComment(new Comment("Jane Smith", "Very helpful, thanks!"));
+
+        videos[1].AddComment(new Comment("Alice Johnson", "So many good tips!"));
+        videos[1].AddComment(new Comment("Bob Brown", "Clear and concise."));
+
+        videos[2].AddComment(new Comment("Charlie Davis", "Now I get interfaces!"));
+        videos[2].AddComment(new Comment("Dana White", "Perfect explanation."));
+
+        foreach (var video in videos) {
+            video.DisplayVideoInfo();
+            Console.WriteLine();
+        }
     }
 }
---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+class Video {
+    protected string _title;
+    protected string _author;
+    private int _length;
+    private List<Comment> comments = new List<Comment>();
+
+    public Video(string title, string author, int length) {
+        _title = title;
+        _author = author;
+        _length = length;
+    }
+
+    public void AddComment(Comment comment) {
+        comments.Add(comment);
+    }
+
+    public int GetNumberOfComments() {
+        return comments.Count;
+    }
+
+    public void DisplayVideoInfo() {
+        Console.WriteLine($"Title: {_title}, Author: {_author}, Length: {_length} seconds");
+        Console.WriteLine("Comments:");
+        foreach (var comment in comments) {
+            Console.WriteLine($"- {comment.GetName()}: {comment.GetText()}");
+        }
+    }
+}
