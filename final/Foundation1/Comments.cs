4786db7b20e668de01e0fa367f860b3c8fa55e6f
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2024-05-29 14:13:50 +0100

    Final F1

--- final/Foundation1/Comments.cs
+++ final/Foundation1/Comments.cs
@@ -5,7 +5,8 @@ class Comment {
     private string _name;
     private string _text;
 
-    public Comment(string name, string text) {
+    public Comment(string name, string text)
+    {
         _name = name;
         _text = text;
     }
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -7,24 +7,29 @@ class Video {
     protected int _length;
     private List<Comment> comments = new List<Comment>();
 
-    public Video(string title, string author, int length) {
+    public Video(string title, string author, int length) 
+    {
         _title = title;
         _author = author;
         _length = length;
     }
 
-    public void AddComment(Comment comment) {
+    public void AddComment(Comment comment) 
+    {
         comments.Add(comment);
     }
 
-    public int GetNumberOfComments() {
+    public int GetNumberOfComments()
+    {
         return comments.Count;
     }
 
-    public void DisplayVideoInfo() {
+    public void DisplayVideoInfo() 
+    {
         Console.WriteLine($"\nTitle: {_title}, Author: {_author}, Length: {_length} seconds");
         Console.WriteLine("Comments:");
-        foreach (var comment in comments) {
+        foreach (var comment in comments) 
+        {
             Console.WriteLine($"- {comment.GetName()}: {comment.GetText()}");
         }
     }
