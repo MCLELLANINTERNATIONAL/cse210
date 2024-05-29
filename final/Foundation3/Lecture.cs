30c248db73658c4e50686585db075f455645d3ac
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2024-05-29 14:24:53 +0100

    Final F3

--- final/Foundation3/Lecture.cs
+++ final/Foundation3/Lecture.cs
@@ -1,18 +1,20 @@
 using System;
 
-// Derived class for Receptions
-class Reception : Event
+// Derived class for Lectures
+class Lecture : Event
 {
-    protected string _rsvpEmail;
+    protected string _speaker;
+    protected int _capacity;
 
-    public Reception(string title, string description, DateTime date, TimeSpan time, Address address, string rsvpEmail)
+    public Lecture(string title, string description, DateTime date, TimeSpan time, Address address, string speaker, int capacity)
         : base(title, description, date, time, address)
     {
-        _rsvpEmail = rsvpEmail;
+        _speaker = speaker;
+        _capacity = capacity;
     }
 
     public override string GetFullDetails()
     {
-        return $"{base.GetFullDetails()}\nType: Reception\nRSVP Email: {_rsvpEmail}";
+        return $"{base.GetFullDetails()}\nType: Lecture\nSpeaker: {_speaker}\nCapacity: {_capacity}";
     }
 }
---
+++ final/Foundation3/Reception.cs
@@ -0,0 +1,18 @@
+using System;
+
+// Derived class for Receptions
+class Reception : Event
+{
+    protected string _rsvpEmail;
+
+    public Reception(string title, string description, DateTime date, TimeSpan time, Address address, string rsvpEmail)
+        : base(title, description, date, time, address)
+    {
+        _rsvpEmail = rsvpEmail;
+    }
+
+    public override string GetFullDetails()
+    {
+        return $"{base.GetFullDetails()}\nType: Reception\nRSVP Email: {_rsvpEmail}";
+    }
+}
