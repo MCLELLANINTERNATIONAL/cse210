283ce9cd3407282e6aa714ebd3fd5f8e47bfc509
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2024-05-31 00:11:42 +0100

    Final F3

--- final/Foundation3/Address.cs
+++ final/Foundation3/Address.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 
 // Define an address class
 class Address
@@ -6,18 +7,20 @@ class Address
     protected string _street;
     protected string _city;
     protected string _state;
+    protected string _country;
     protected string _zipCode;
 
-    public Address(string street, string city, string state, string zipCode)
+    public Address(string street, string city, string state, string country, string zipCode)
     {
         _street = street;
         _city = city;
         _state = state;
+        _country = country;
         _zipCode = zipCode;
     }
 
     public override string ToString()
     {
-        return $"{_street}, {_city}, {_state}, {_zipCode}";
+        return $"{_street}, {_city}, {_state}, {_country}, {_zipCode}";
     }
 }
