90d905d55745fa0e8f9c245f1b927c5f20f166c3
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2024-06-03 08:01:49 +0100

    Final

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
@@ -6,9 +7,9 @@ class Program
     {
        Event[] events = new Event[]
         {
-            new Lecture("Astrophysics 101", "Explore the stars!", new DateTime(2024, 5, 15), new TimeSpan(18, 0, 0), new Address("123 Galaxy St", "SpaceTown", "Universe", "00001"), "Dr. Cosmos", 100),
-            new Reception("Annual Charity Ball", "A night of elegance and giving.", new DateTime(2024, 6, 20), new TimeSpan(19, 30, 0), new Address("1000 Mountain Drive", "Gotham", "NY", "11001"), "rsvp@charityball.com"),
-            new OutdoorGathering("Beach Volleyball", "Fun in the sun with a chance of rain.", new DateTime(2024, 7, 12), new TimeSpan(10, 0, 0), new Address("555 Ocean Blvd", "Sunnyville", "CA", "90210"), "Sunny with occasional clouds")
+            new Lecture("Astrophysics 101", "Explore the stars!", new DateTime(2024, 5, 15), new TimeSpan(18, 0, 0), new Address("123 Galaxy St", "SpaceTown", "Universe", "USA", "00001"), "Dr. Cosmos", 100),
+            new Reception("Annual Charity Ball", "A night of elegance and giving.", new DateTime(2024, 6, 20), new TimeSpan(19, 30, 0), new Address("1000 Mountain Drive", "Gotham", "NY", "USA", "11001"), "rsvp@charityball.com"),
+            new OutdoorGathering("Beach Volleyball", "Fun in the sun with a chance of rain.", new DateTime(2024, 7, 12), new TimeSpan(10, 0, 0), new Address("555 Ocean Blvd", "Sunnyville", "CA", "USA", "90210"), "Sunny with occasional clouds")
         };
 
         foreach (Event evt in events)
