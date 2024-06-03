ce3885d358a510f4e21c3a868e4c685be2f339bb
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2024-06-03 08:14:17 +0100

    Final

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -21,7 +21,7 @@ public abstract class Activity
     public abstract double GetPace();
 
     // Virtual method for generating a summary of the activity
-    public string GetSummary()
+    public virtual string GetSummary()
     {
         return $"\n{_date:dd MMM yyyy} - {GetType().Name} ({_durationInMinutes} min) - Distance: {GetDistance():0.0} km, Speed: {GetSpeed():0.0} kph, Pace: {GetPace():0.0} min per km\n";
     }
